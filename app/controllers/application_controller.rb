4d2de89937c8e8184b1640af3de1249e54d4b4c8
eamesa <eamadorm@chicagobooth.edu>
2022-02-01 17:30:54 +0000

    calc add done

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,15 @@
 class ApplicationController < ActionController::Base
 
+  def add_form
+    render({ :template => "calculation_templates/add_form.html.erb"})
+  end
+
+def add_results
+  @first_num = params.fetch("first_number").to_f
+  @second_num = params.fetch("second_number").to_f
+
+  render({ :template => "calculation_templates/add_results.html.erb"})
+end
+
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
 
+get("/add",{:controller => "application", :action => "add_form"})
+
+get("/add_results",{:controller => "application", :action => "add_results"})
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
